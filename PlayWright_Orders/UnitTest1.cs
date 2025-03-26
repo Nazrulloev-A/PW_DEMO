61426430ba7ff63d0b8980d74df593d51d09335b
Amrullo <smith2011@inbox.ru>
2025-03-26 09:40:37 -0500

    playwright basic..

---
+++ PlayWright_Orders/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Playwright;
+
+namespace PlayWright_Orders;
+
+public class UnitTest1
+{
+    [Fact]
+    public async Task Test1()
+    {
+       using var playwright = await Playwright.CreateAsync();
+         await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+         {
+              Headless = false
+         });
+            var page = await browser.NewPageAsync();
+            await page.GotoAsync("http://secure.smartbearsoftware.com/samples/testcomplete12/weborders/");
+           
+            await page.FillAsync("input[name='ctl00$MainContent$username']", "Tester");
+            await page.FillAsync("input[name='ctl00$MainContent$password']", "test");
+            await page.ClickAsync("input[name='ctl00$MainContent$login_button']");
+            await page. ScreenshotAsync(new PageScreenshotOptions 
+                { Path = "screenshot.png" });
+    }
+}
